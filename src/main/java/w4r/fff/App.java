bc2a076a42eaf5564dfe27b7a613381895f6daa0
ArtyrG <artyr_galeev@mail.ru>
2016-10-15 17:27:24 +0300

    commit для 1 верки Metod1

--- src/main/java/w4r/fff/App.java
+++ src/main/java/w4r/fff/App.java
@@ -5,6 +5,10 @@ public class App
 {
     public static void main( String[] args )
     {
+<<<<<<< 1535ef420098c0d599a755913a387e467bcae12a
        //для полного понимания
+=======
+       //sdfsdfsdf
+>>>>>>> b4d08bf6abe9e1f1fe50d7a42c3ae1e592bb2187
     }
 }
